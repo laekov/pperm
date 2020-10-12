9b878b880052d6a9aaeee5f9eec65db05bbcb146
Xiaozhi Wang <wangxz20@mails.tsinghua.edu.cn>
2020-10-13 01:17:04 +0800

    Add lexicographical order

---
+++ src/lex.cc
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <iostream>
+#include <pperm.hh>
+
+class Lex: public PermAlgorithm {
+private:
+	int *a;
+
+protected:
+	virtual void setup_() {
+		a = new int[n];
+		for (int i = 0; i < n; ++i)
+			a[i] = i;
+	}
+	virtual void generate_() {
+		int s = 0;
+        while(1) {
+            for (int i(0); i<n; i++)
+                printf("%d ",a[i]);
+            puts("");
+            bool Flag=false;
+            for (int i(n-2); i>=0; i--)
+                if (a[i] < a[i+1]){
+                    int x=i+1;
+                    for (int j(i+2);j<n;j++)
+                        if (a[j] > a[i] && a[j] < a[x])
+                            x = j;
+                    Flag=true;
+                    std::swap(a[i], a[x]);
+                    int R=n-1;
+                    for (int L(i+1); L < R;)
+                        std::swap(a[L++],a[R--]);
+                    break;
+                }
+            if(!Flag)
+                break;
+        }
+	}
+};
+
+REGISTER_PERM_ALGORITHM("lex", Lex);
