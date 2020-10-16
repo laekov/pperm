e9a2005048d071b8e68698b5884447d4e6226c5f
Rick Ho <laekov.h@gmail.com>
2020-10-16 15:05:20 +0800

    parallel benchmarking script

---
+++ scripts/gpu_performance.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+gen() {
+	for t in {8..15}
+	do
+		for algo in recr lex sjt
+		do
+			echo $t $algo
+		done
+	done
+}
+
+mkdir -p logs
+
+gen | xargs -P 8 -n 2 --process-slot-var=WORKER_ID \
+	bash -c 'CUDA_VISIBLE_DEVICES=$WORKER_ID \
+	numactl -C $(expr $WORKER_ID \* 4) \
+	./pperm -l $0 $1_gpu | tee logs/l_$0_algo_$1_gpu.log'
